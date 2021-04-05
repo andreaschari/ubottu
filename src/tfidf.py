89cddcc2aa97dd52fbf2b851c82b29c37295fd41
Andreas Chari <andreas9810@outlook.com>
2021-04-05 16:24:12 +0100

    Update tfidf.py

--- src/tfidf.py
+++ src/tfidf.py
@@ -8,10 +8,11 @@ from sklearn.feature_extraction.text import *
 from sklearn.metrics import *
 from sklearn.preprocessing import *
 from sklearn.svm import *
+import pandas as pd
 
-TRAIN_FILES = ['../data/trainset%s.csv' % s for s in ['_full']]
-VAL_FILE = '../data/valset.csv'
-TEST_FILE = '../data/testset.csv'
+TRAIN_FILE = '/content/ubottu/src/ubuntu_csvfiles/trainset.csv'
+VAL_FILE = '/content/ubottu/src/ubuntu_csvfiles/valset.csv'
+TEST_FILE = '/content/ubottu/src/ubuntu_csvfiles/testset.csv'
 
 def recall(probas, k, group_size):    
     n_batches = len(probas) // group_size
@@ -49,27 +50,40 @@ def run(C_vec, R_vec, Y, group_size):
     print "Y=1: ", np.sum(pred)
     print classification_report(YY, pred)
     
-def load_data(fname):
-    C = []
-    R = []
-    Y = []
-    for line in csv.reader(open(fname), delimiter=','):
-        C.append(line[0])
-        R.append(line[1])
-        Y.append(int(line[2]))
+def load_data(dataframe):
+    C = dataframe['context'].to_list()
+    R = dataframe['response'].to_list()
+    # need to also convert flag to int
+    Y = res = list(map(int, dataframe['flag'].to_list()))
+
     return C, R, Y
 
-val_C, val_R, val_Y = load_data(VAL_FILE)
-test_C, test_R, test_Y = load_data(TEST_FILE)
-for train_file in TRAIN_FILES:
-    print train_file
-    train_C, train_R, train_Y = load_data(train_file)
+val_file_df = pd.read_csv(VAL_FILE, names=['context', 'response', 'flag'])
+test_file_df = pd.read_csv(TEST_FILE,  names=['context', 'response', 'flag'])
+print 'read csv files'
+
+val_file_df = val_file_df.fillna(' ')
+test_file_df = test_file_df.fillna(' ')
+
+val_C, val_R, val_Y = load_data(val_file_df)
+print 'loaded val data!'
+test_C, test_R, test_Y = load_data(test_file_df)
+print 'loaded test data!'
+
+for train_file_chunk in pd.read_csv(TRAIN_FILE,  names=['context', 'response', 'flag'], chunksize=10000) :
+    train_file_chunk = train_file_chunk.fillna(' ')
+    train_C, train_R, train_Y = load_data(train_file_chunk)
+    print 'loaded train file batch!'
+    
     vectorizer = TfidfVectorizer()
+    print 'training Vectorizer'
     vectorizer.fit(train_C+train_R+val_C+val_R)
+    
     C_vec = vectorizer.transform(test_C)
     R_vec = vectorizer.transform(test_R)
     Y = np.array(test_Y)
     
     for group_size in [2, 10]:
-        print train_file, group_size
         run(C_vec, R_vec, Y, group_size)
+
+print 'DONE!'
